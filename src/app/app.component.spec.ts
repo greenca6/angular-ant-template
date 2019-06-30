9269d41495132c95ead611ad0f8aa6915403a237
craig-green <CGreen@sbd2.com>
2019-06-30 16:48:50 -0400

    Add test using mock service pattern

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,8 +3,21 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { Observable, of } from 'rxjs';
+import { HelloWorldService } from './services/hello-world.service';
+
+const mockResponse = require('../../__mocks__/hello-world.json');
+
+class MockHelloWorldService {
+  getResponse(): Observable<any> {
+    return of(mockResponse);
+  }
+}
 
 describe('AppComponent', () => {
+  let fixture;
+  let app;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -16,14 +29,23 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+    }).overrideComponent(AppComponent, {
+      set: {
+        providers: [
+          { provide: HelloWorldService, useClass: MockHelloWorldService },
+        ],
+      },
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
+  })
 
-  // TODO: catch event scenario
+  it('should create the app', () => {
+    expect(app).toBeTruthy();
+    console.log(app.response);
+  });
 });
